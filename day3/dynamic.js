f90c7eed1b11e5ef5ca309c1e91d4f5fc7ae886b
Rishav-009 <rishavsanjorian789@gmail.com>
2026-05-28 21:41:17 +0530

    added html structure

--- day3/dynamic.js
+++ day3/dynamic.js
@@ -1,50 +1,60 @@
 let btn = document.querySelector("#btn");
-
-btn.addEventListener("click",function(){
-    let inp = document.querySelector("#input").value;
-    
-    let li = document.createElement("li");
-    let del = document.createElement("button");
-    del.innerText= "delete";
-    li.innerText= inp;
-    li.style.color="red";
-    
-
-    let done = document.createElement("button");
-    done.innerText = "Completed";
-
-    
-
-    let divv = document.createElement("div");
-    divv.style.display ="flex";
-    divv.style.gap= "10px";
-    divv.append(li,del,done);
-
-    
-
-    del.addEventListener("click",function(){
-        divv.remove();
-     })
-
-    done.addEventListener("click",function(){
-        //li.innerText.style.color="green";
-        if(done.innerText=="Completed"){
-            done.innerText="DONE";
-            done.style.color = "green";
-            li.style.color = "green";
-            li.style.textDecoration = "line-through";
+function addTask(){
+        let inp = document.querySelector("#input").value;
+        if(inp.trim()==""){
+            alert("Please Enter Task To Be Added");
+            return;
         }
-        else{
-            done.innerText="Completed";
-            li.style.textDecoration="none";
-             done.style.color = "red";
-            li.style.color = "red";
-
-        }
+        let li = document.createElement("li");
+        let del = document.createElement("button");
+        del.innerText= "delete";
+        li.innerText= inp;
+        li.style.color="red";
         
-    })
-    document.querySelector("#container").append(divv);
 
-    document.querySelector("#input").value=" ";
-    
+        let done = document.createElement("button");
+        done.innerText = "Completed";
+
+        
+
+        let divv = document.createElement("div");
+        divv.style.display ="flex";
+        divv.style.gap= "10px";
+        divv.append(li,del,done);
+
+        
+
+        del.addEventListener("click",function(){
+            divv.remove();
+        })
+
+        done.addEventListener("click",function(){
+            //li.innerText.style.color="green";
+            if(done.innerText=="Completed"){
+                done.innerText="DONE";
+                done.style.color = "green";
+                li.style.color = "green";
+                li.style.textDecoration = "line-through";
+            }
+            else{
+                done.innerText="Completed";
+                li.style.textDecoration="none";
+                done.style.color = "red";
+                li.style.color = "red";
+
+            }
+            
+        })
+        document.querySelector("#container").append(divv);
+
+        document.querySelector("#input").value=" ";
+    }
+btn.addEventListener("click",function(){
+    addTask();
+})
+let input = document.querySelector("#input");
+input.addEventListener("keydown",function(event){
+    if(event.key == "Enter"){
+        addTask();
+    }
 })
