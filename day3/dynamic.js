209262aa2a203902ba9414eb7a8edbda960b1823
Rishav-009 <rishavsanjorian789@gmail.com>
2026-05-28 19:48:58 +0530

    Successfully added the toggle behaviour to todo app

--- day3/dynamic.js
+++ day3/dynamic.js
@@ -7,16 +7,44 @@ btn.addEventListener("click",function(){
     let del = document.createElement("button");
     del.innerText= "delete";
     li.innerText= inp;
+    li.style.color="red";
+    
+
+    let done = document.createElement("button");
+    done.innerText = "Completed";
+
+    
 
     let divv = document.createElement("div");
     divv.style.display ="flex";
     divv.style.gap= "10px";
-    divv.append(li,del);
+    divv.append(li,del,done);
+
+    
 
     del.addEventListener("click",function(){
         divv.remove();
      })
+
+    done.addEventListener("click",function(){
+        //li.innerText.style.color="green";
+        if(done.innerText=="Completed"){
+            done.innerText="DONE";
+            done.style.color = "green";
+            li.style.color = "green";
+            li.style.textDecoration = "line-through";
+        }
+        else{
+            done.innerText="Completed";
+            li.style.textDecoration="none";
+             done.style.color = "red";
+            li.style.color = "red";
+
+        }
+        
+    })
     document.querySelector("#container").append(divv);
 
     document.querySelector("#input").value=" ";
+    
 })
