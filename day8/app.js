568b8098bfd463889ca0b81184443cb929bb862e
Rishav-009 <rishavsanjorian789@gmail.com>
2026-07-16 14:28:41 +0530

    complete js ES  modules with named and default exports

---
+++ day8/app.js
@@ -0,0 +1,5 @@
+import {add,subract} from "./maths.js";
+import calculator from "./maths.js";
+console.log(add(10,20));
+console.log(subract(20,10));
+console.log(calculator(1111,11));
---
+++ day8/maths.js
@@ -0,0 +1,11 @@
+function add(a ,b){
+    return a + b; 
+}
+function subract (a,b){
+    return a-b;
+}
+function calculator(a,b){
+    return a / b ;
+}
+export default calculator;//default export 
+export {add,subract};// named export
